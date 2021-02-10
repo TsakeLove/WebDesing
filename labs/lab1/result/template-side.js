97e7fe7600f596ebc14ea1ecc5832775d5adeb69
Ihor <tsake.love@gmail.com>
2021-02-10 20:45:59 +0200

    start

---
+++ labs/lab1/result/template-side.js
@@ -0,0 +1,15 @@
+document.write(" <div class=\"child-sidebar\">\n" +
+    "                    <div class=\"child-nav\">\n" +
+    "                        <ul>\n" +
+    "                            <li><a href=\"problem.html\">Постановка задачі</a></li>\n" +
+    "                            <li><a href=\"environment.html\">Середовище розробки сайту. Місце розташування сайту</a></li>\n" +
+    "                            <li><a href=\"description.html\">Опис предметного середовища</a></li>\n" +
+    "                            <li><a href=\"codes.html\">Зовнішний вигляд сайту. НТМL-коди файлів.</a></li>\n" +
+    "                            <li><a href=\"tables.html\">HTML-код ТАБЛИЦЬ – скриншот результатувиведення таблиці</a></li>\n" +
+    "                            <li><a href=\"forms.html\">HTML-код ФОРМИ</a></li>\n" +
+    "                            <li><a href=\"images.html\">HTML-код ЗОБРАЖЕННЯ</a></li>\n" +
+    "                            <li><a href=\"literature.html\">Використана література, сайти, при виконанні лабораторної роботи №1</a></li>\n" +
+    "                            <li><a href=\"findings.html\">ВИСНОВКИ</a></li>\n" +
+    "\n" +
+    "                        </ul>\n" +
+    "                    </div>");
---
+++ labs/lab1/result/wrapper.js
@@ -0,0 +1,18 @@
+document.write("<div class=\"header-wrapper\">\n" +
+    "        <h1><a href='../../../index.html'>  Сторінка для звітів <br>\n" +
+    "                    Студента групи ІП-82 Цакєлова Ігора Ігоровича\n" +
+    "                </a></h1>\n" +
+    "    </div>\n" +
+    "    <div class=\"side-wrapper\">\n" +
+    "        <ul>\n" +
+    "            <li><a href=\"problem.html\">1</a></li>\n" +
+    "            <li><a href=\"#\">2</a></li>\n" +
+    "            <li><a href=\"#\">3</a></li>\n" +
+    "            <li><a href=\"#\">4</a></li>\n" +
+    "            <li><a href=\"#\">5</a></li>\n" +
+    "            <li><a href=\"#\">6</a></li>\n" +
+    "            <li><a href=\"#\">7</a></li>\n" +
+    "            <li><a href=\"#\">8</a></li>\n" +
+    "            <li><a href=\"#\">9</a></li>\n" +
+    "        </ul>\n" +
+    "    </div>");
